1e92526e0540cd0b296ecc435c8743e530edd8e7
Earendel <earendelentertainment@gmail.com>
2016-10-23 16:49:46 +0100

    fix blank slot size

--- prototype/entity/entity.lua
+++ prototype/entity/entity.lua
@@ -81,7 +81,7 @@ for i1, material in ipairs(materials) do
 				circuit_wire_max_distance = 0
 			}
 			if(symbol == "blank") then -- add constant slot
-				item_slot_count = 1
+				entity.item_slot_count = 1
 			end
 			if(size == "large") then
 				entity.corpse = "medium-remnants"
